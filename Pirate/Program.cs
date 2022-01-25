5b301a155aed359b2a801891384e7240c9578a5a
Elliot Walker <Elliot.Walker@elev.ga.ntig.se>
2022-01-25 10:28:51 +0100

    Fundamentals first

--- Pirate/Program.cs
+++ Pirate/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using Raylib_cs;
+using System.Numerics;
 
-namespace Pirate
+WriteCentered("You Are About to Start The Pirate Game!");
+WriteCentered("Press Enter To Load In");
+
+static void WriteCentered(string text)
 {
-    class Program
+    int numberOfSpaces = Console.WindowWidth / 2 - text.Length /2;
+
+    int i = 0;
+    while (i < numberOfSpaces)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        i++;
+        Console.Write(" ");
     }
+    Console.WriteLine(text);
 }
+
+Console.ReadLine();
+
+Raylib.InitWindow(800,600, "Pirate");
+Raylib.SetTargetFPS(60);
+
+Texture2D playerImage = Raylib.LoadTexture("pirate.png");
+Rectangle playerRect = new Rectangle(15, 300, playerImage.width, playerImage.height);
+
+string level = "stage1";
+
+while (!Raylib.WindowShouldClose())
+{
+
+}
+
+Raylib.BeginDrawing();
+{
+
+if (level == "stage1" || level == "stage2")
+{
+    Raylib.ClearBackground(Color.WHITE);
+    Raylib.DrawTexture(playerImage, (int)playerRect.x, (int)playerRect.y, Color.WHITE);
+}     
+
+}
+
+Raylib.EndDrawing();
