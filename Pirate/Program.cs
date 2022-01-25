6c52e3c1d3c03275c56eaa67d03c297c0ebaf203
Elliot Walker <Elliot.Walker@elev.ga.ntig.se>
2022-01-25 10:38:58 +0100

    Included movement to the playerRect

--- Pirate/Program.cs
+++ Pirate/Program.cs
@@ -26,11 +26,34 @@ Raylib.SetTargetFPS(60);
 Texture2D playerImage = Raylib.LoadTexture("pirate.png");
 Rectangle playerRect = new Rectangle(15, 300, playerImage.width, playerImage.height);
 
+float speed = 3.5f;
+
+bool moveX = false;
+bool moveY = false;
+
+Vector2 movement = new Vector2();
+
 string level = "stage1";
 
 while (!Raylib.WindowShouldClose())
 {
+    moveX = false;
+    moveY = false;
+    
+    if (level == "stage1" || level == "stage2")
+    {
+        movement = ReadMovement(speed);
+        playerRect.x += movement.X;
+        playerRect.y += movement.Y;
 
+            if (playerRect.x < 0 || playerRect.x + playerRect.width > Raylib.GetScreenWidth())
+    {
+        moveX = true;
+    }
+    if (playerRect.y < 0 || playerRect.y + playerRect.height > Raylib.GetScreenHeight())
+    {
+        moveY = true;
+    }
 }
 
 Raylib.BeginDrawing();
@@ -45,3 +68,16 @@ if (level == "stage1" || level == "stage2")
 }
 
 Raylib.EndDrawing();
+
+static Vector2 ReadMovement(float speed)
+{
+    Vector2 movement = new Vector2();
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) movement.Y = -speed;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_S)) movement.Y = speed;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) movement.X = -speed;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_D)) movement.X = speed;     
+
+    return movement;
+}
+}
+
